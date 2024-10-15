1e94406e0439cb8286204c78028e6d98aa0aac44
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-15 17:45:34 +0000

    deploy

--- process_form.php
+++ process_form.php
@@ -35,8 +35,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $body .= "Mensagem: $message\n";
 
             // Adiciona cabeçalhos para o e-mail
-            $headers = "From: $email\r\n";
-            $headers .= "Reply-To: $email\r\n";
+            $headers = "From: $emailCli\r\n";
+            $headers .= "Reply-To: $emailCli\r\n";
             $headers .= "Content-Type: text/plain; charset=UTF-8\r\n"; // Define a codificação como UTF-8
             $headers .= "MIME-Version: 1.0\r\n";
 
