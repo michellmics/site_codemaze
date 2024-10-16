958496e07eca11452ba13bf32d85a8d76500ac09
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 03:11:41 +0000

    deploy

--- admin/alter_senha_proc.php
+++ admin/alter_senha_proc.php
@@ -14,7 +14,7 @@ class LoginSystem extends SITE_ADMIN
                 $this->conexao();
             }
 
-            $this->getUserInfoById(1);
+            $this->getUserInfoById(4);
             var_dump($this->ARRAY_USERINFOBYID);
             echo "teste 2";
             die();
