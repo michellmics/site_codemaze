dcfce8caa003bbc369622472db38dec970734209
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 02:46:22 +0000

    deploy

---
+++ admin/alter_senha.php
@@ -0,0 +1,127 @@
+<?php
+include_once 'objetos.php';
+  
+session_start(); 
+define('SESSION_TIMEOUT', 1800); // 30 minutos
+
+if (!isset($_SESSION['user_id'])) 
+{
+  header("Location: index.php");
+  exit();
+}
+
+$siteAdmin = new SITE_ADMIN();
+$siteAdmin->getSiteInfo(); 
+$siteAdmin->getUserInfo();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Administração - Site <?php echo htmlspecialchars($siteAdmin->ARRAY_SITEINFO["SBI_DCDOMAINSITE"]); ?></title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <img src="../images/logos/logocodemaze_preto.png" alt="Logo" />
+        </div>
+
+        <div class="register-box-body">
+            <p class="login-box-msg">Alteração de senha</p>
+            <form action="alter_senha_proc.php" method="post" id="formRegistro">
+              >
+
+                <div class="form-group has-feedback">
+                    <input type="password" name="senha" class="form-control" placeholder="Digite a senha atual" maxlength="20" required/>
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" name="novasenha" class="form-control" placeholder="Digite a nova senha" maxlength="20" required/>
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input type="password" name="novasenha_repeat" class="form-control" placeholder="Digite a senha novamente" maxlength="20" required/>
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck"></div>                        
+                    </div><!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="button" onclick="validarFormulario()" class="btn btn-primary btn-block btn-flat">Registrar</button>
+                    </div><!-- /.col -->
+                </div>
+            </form>
+
+            <script>
+                function validarFormulario() {
+                    const nome = document.querySelector('input[name="nome"]').value.trim();
+                    const email = document.querySelector('input[name="email"]').value.trim();
+                    const sexo = document.querySelector('select[name="sexo"]').value;
+                    const senha = document.querySelector('input[name="senha"]').value.trim();
+
+                    if (!novasenha || !novasenha_repeat || !senha) {
+                        alert("Todos os campos devem ser preenchidos.");
+                        return false;
+                    }
+
+                    // Expressão regular para validar a senha
+                    const senhaRegex = /^(?=.*[A-Z])(?=.*[\W_])(?=.{8,})/; // Pelo menos 8 caracteres, uma letra maiúscula, um caractere especial
+                    if (!senhaRegex.test(senha)) {
+                        alert("A senha deve ter pelo menos 8 caracteres, incluir pelo menos uma letra maiúscula e um caractere especial.");
+                        return false;
+                    }
+
+                    // Expressão regular para validar a senha
+                    const senhaRegex = /^(?=.*[A-Z])(?=.*[\W_])(?=.{8,})/; // Pelo menos 8 caracteres, uma letra maiúscula, um caractere especial
+                    if (!senhaRegex.test(novasenha)) {
+                        alert("A senha deve ter pelo menos 8 caracteres, incluir pelo menos uma letra maiúscula e um caractere especial.");
+                        return false;
+                    }
+
+                    // Envia o formulário manualmente após a validação
+                    document.getElementById('formRegistro').submit();
+                }
+            </script>
+        </div><!-- /.form-box -->
+    </div><!-- /.register-box -->
+
+    <!-- jQuery 2.1.3 -->
+    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- iCheck -->
+    <script src="plugins/iCheck/icheck.min.js"></script>
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+</body>
+</html>
