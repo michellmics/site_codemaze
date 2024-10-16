cdd06ac1efc36f93fd6d2613ea561e547b83761d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 00:51:38 +0000

    deploy

--- admin/register.php
+++ admin/register.php
@@ -46,7 +46,7 @@
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" name="sexo" class="form-control" placeholder="Sexo"/>
+            <input type="text" name="sexo" class="form-control" placeholder="Sexo"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
