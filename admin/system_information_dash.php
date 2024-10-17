5e4ed34454b7b9bc3eb1a422338236f6a3482aa3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 19:08:10 +0000

    deploy

--- admin/system_information_dash.php
+++ admin/system_information_dash.php
@@ -1,12 +1,32 @@
 <?php
 include_once 'objetos_status_server.php'; // Carrega a classe de conexão e objetos
 
-$swhmcpanel_info = new WHMCPANEL_STATUS();
-$result = $swhmcpanel_info->getDiscUsage();
+//$swhmcpanel_info = new WHMCPANEL_STATUS();
+//$result = $swhmcpanel_info->getDiscUsage();
 
 var_dump($result);
 
+$token = "L12T1GH3J4AD272VVQMX3WTN6RAUBRAZ"; // Substituir pelo token gerado
+$username = "inartcom"; // Usuário da conta cPanel
 
+$url = "https://localhost:2083/cpsess1234567890/execute/DomainInfo/get_ip_address";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Evita problemas com SSL
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Authorization: cpanel $username:$token"
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+if ($response) {
+    $data = json_decode($response, true);
+    echo "Endereço IP: " . $data['data']['ip'] ?? 'Erro';
+} else {
+    echo "Falha na requisição.";
+}
 
 
 ?>
