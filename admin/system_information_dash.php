ed0f056d2050552de314e9f6bcf6a6e624c30041
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 20:07:13 +0000

    deploy

--- admin/system_information_dash.php
+++ admin/system_information_dash.php
@@ -7,10 +7,10 @@ include_once 'objetos_status_server.php'; // Carrega a classe de conexão e obje
 
 
 
-$user = "codemaze";
+$user = "inartcom";
     $token = "G3T065AP3A15QZ22FKYSF7NO30Y5ROT4";
 
-    $query = "https://127.0.0.1:2087/json-api/acctsummary?api.version=1";
+    $query = "https://127.0.0.1:2087/json-api/loadavg?api.version=1";
 
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
@@ -22,7 +22,7 @@ $user = "codemaze";
     curl_setopt($curl, CURLOPT_URL, $query);
 
     $result = curl_exec($curl);
- 
+
     $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     if ($http_status != 200) {
         echo "[!] Error: " . $http_status . " returned\n";
