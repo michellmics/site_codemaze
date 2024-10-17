f844f7d92256f78c44527f4a16a2ea0ea389463f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 21:14:56 +0000

    deploy

--- admin/system_information_dash.php
+++ admin/system_information_dash.php
@@ -15,7 +15,7 @@ $user = "inartcom";
 
     //$query = "https://127.0.0.1:2087/json-api/gethostname?api.version=1";
     //$query = "https://127.0.0.1:2087/json-api/loadavg?api.version=1";
-    $query = "https://127.0.0.1:2087/json-api/servicestatus?api.version=1";
+    $query = "https://127.0.0.1:2087/json-api/get_disk_usage?api.version=1";
 
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
