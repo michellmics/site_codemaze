e3ae94ba8275686aa3cc44913152c5c8d376c72f
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 00:23:04 -0300

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -341,7 +341,7 @@
             </li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-edit"></i> <span>Parametros</span>
+                <i class="fa fa-edit"></i> <span>Parâmetros</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
