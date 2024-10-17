f477221cba549e4fda3aa51cfee78ac436978a2e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 18:14:47 +0000

    deploy

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -433,7 +433,7 @@
 
       <footer class="footer">
         <div class="container text-left">
-        <a href="https://codemaze.com.br" target="_blank">-&nbsp;&nbsp;&nbsp;&nbsp;Website Codemaze - Login Realizado ás <? echo date('d/m/Y H:i:s'); ?></a>
+        <a href="https://codemaze.com.br" target="_blank">-&nbsp;&nbsp;&nbsp;&nbsp;Website Codemaze</a> - Login Realizado ás <? echo date('d/m/Y H:i:s'); ?>
         </div>
     </footer>
 
