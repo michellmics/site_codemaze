5c214a685823c228f81390e2f087735df51f159e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 22:39:06 -0300

    Update form_cliente.php

--- admin/form_cliente.php
+++ admin/form_cliente.php
@@ -152,7 +152,19 @@
 			</div>
 			</div>
 		</div>
-        	<!-- ENDEREÇO  ESTADO  CIDADE-->			
+        	<!-- ENDEREÇO  ESTADO  CIDADE-->
+		<!-- OBSERVAÇÕES-->
+		<div style="width: 100%; margin-bottom: 20px;">
+			<div class="form-group" style="display: flex; gap: 10px; align-items: center; flex-wrap: wrap;">
+			<div class="form-group">
+                      	<label>OBSERVAÇÕES</label>
+                      	<textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    	</div>
+			</div>
+		</div>
+		<!-- OBSERVAÇÕES-->
+			
+                 
 
 
 
