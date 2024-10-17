2ee1d21d257d052c17acfea86214e0a93138346c
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 22:49:08 -0300

    Update form_cliente.php

--- admin/form_cliente.php
+++ admin/form_cliente.php
@@ -144,7 +144,36 @@
 			</div>
 			<div style="flex: 2; min-width: 100px;">
 			<label>ESTADO</label>
-			<input type="text" style="width: 70%;" maxlength="2" class="form-control" placeholder="Enter ..." name="descricao_2" value="<?php echo htmlspecialchars($descEmpresa_2["PAD_DCTITLE"]); ?>" />
+			<select class="form-control" name="estado" style="width: 100%;">
+                	<option value="" disabled selected>Selecione...</option>
+                	<option value="AC">AC</option>
+                	<option value="AL">AL</option>
+                	<option value="AP">AP</option>
+                	<option value="AM">AM</option>
+                	<option value="BA">BA</option>
+                	<option value="CE">CE</option>
+                	<option value="DF">DF</option>
+                	<option value="ES">ES</option>
+                	<option value="GO">GO</option>
+                	<option value="MA">MA</option>
+                	<option value="MT">MT</option>
+                	<option value="MS">MS</option>
+                	<option value="MG">MG</option>
+                	<option value="PA">PA</option>
+                	<option value="PB">PB</option>
+                	<option value="PR">PR</option>
+                	<option value="PE">PE</option>
+                	<option value="PI">PI</option>
+                	<option value="RJ">RJ</option>
+                	<option value="RN">RN</option>
+                	<option value="RS">RS</option>
+                	<option value="RO">RO</option>
+                	<option value="RR">RR</option>
+                	<option value="SC">SC</option>
+                	<option value="SP">SP</option>
+                	<option value="SE">SE</option>
+                	<option value="TO">TO</option>
+            		</select>
 			</div>
 			<div style="flex: 4; min-width: 150px;">
 			<label>CIDADE</label>
