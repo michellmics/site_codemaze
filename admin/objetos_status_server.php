48ac625dcfa32204ef342fb7856d5e1a574767b9
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 18:55:43 +0000

    deploy

---
+++ admin/objetos_status_server.php
@@ -0,0 +1,31 @@
+<?php
+
+	class WHMCPANEL_STATUS
+	{
+        //declaração de variaveis 
+        public $token = "4ZES9HJF02MGINRLER9IBHB4J1W36B8A";
+        
+
+
+        public function getDiscUsage($stmtFunction)
+		{		
+			
+            $url = "https://localhost:2087/json-api/getdiskusage?api.version=1";
+            $token = "4ZES9HJF02MGINRLER9IBHB4J1W36B8A";
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Evita problemas com SSL
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Authorization: inartcom:$token"
+            ]);
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            return $response;           
+
+		}	
+	
+
+    }
---
+++ admin/pages/system_information_dash.php
@@ -0,0 +1,11 @@
+<?php
+
+$swhmcpanel_info = new WHMCPANEL_STATUS();
+$result = $swhmcpanel_info->getDiscUsage();
+
+var_dump($result);
+
+
+
+
+?>
