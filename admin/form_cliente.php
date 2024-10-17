f97a7e9adc5f6d346aca93349fcf4a515728f0fd
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 22:24:44 -0300

    Update form_cliente.php

--- admin/form_cliente.php
+++ admin/form_cliente.php
@@ -142,7 +142,7 @@
 			<label>ENDEREÇO</label>
 			<input type="text" style="width: 170%;" maxlength="150" class="form-control" placeholder="Enter ..."  name="descricao_1" value="<?php echo htmlspecialchars($descEmpresa_2["PAD_DCTITLE"]); ?>" />
 			</div>
-			<div style="flex: 1;">
+			<div style="flex: 2;">
 			<label>ESTADO</label>
 			<input type="text" style="width: 30%;" maxlength="2" class="form-control" placeholder="Enter ..." name="descricao_2" value="<?php echo htmlspecialchars($descEmpresa_2["PAD_DCTITLE"]); ?>" />
 			</div>
