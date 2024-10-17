a59498bfddd1e67a124e3e068d35a609690ffa29
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-16 23:25:39 -0300

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -412,7 +412,7 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 2.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        <strong>Todos os direitos reservados &copy; 2024 <a href="#">Agência Codemaze</a>.</strong>
       </footer>
 
     </div><!-- ./wrapper -->
