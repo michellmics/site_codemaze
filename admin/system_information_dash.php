496e72528d1d33161a9f6a041fac991c4b794235
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 21:12:21 +0000

    deploy

--- admin/system_information_dash.php
+++ admin/system_information_dash.php
@@ -13,7 +13,7 @@ include_once 'objetos_status_server.php'; // Carrega a classe de conexão e obje
 $user = "inartcom";
     $token = "G3T065AP3A15QZ22FKYSF7NO30Y5ROT4";
 
-    $query = "https://127.0.0.1:2087/json-api/servicestatus?api.version=1";
+    $query = "https://127.0.0.1:2087/json-api/gethostname?api.version=1";
 
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
