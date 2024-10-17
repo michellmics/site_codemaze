e164dff348986d36de4553f2866a7310636f5c09
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-17 21:51:52 +0000

    deploy

--- admin/objetos_status_server.php
+++ admin/objetos_status_server.php
@@ -9,11 +9,10 @@
 
         public function getInfoWebServer($apiApp)
 	{                       
-	        $query = "https://127.0.0.1:2087/json-api/$apiApp?api.version=1";
-	        //$query = "https://127.0.0.1:2087/json-api/loadavg?api.version=1";
-	        //$query = "https://127.0.0.1:2087/json-api/get_disk_usage?api.version=1";
-	        //$query = "https://127.0.0.1:2087/json-api/showbw?api.version=2";
-                        
+	        //$query = "https://127.0.0.1:2087/json-api/$apiApp?api.version=1";
+	        $query = "https://127.0.0.1:2087/json-api/listaccts?api.version=1&searchtype=status&suspended=1";
+                
+                 
                         
 	        $curl = curl_init();
 	        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
