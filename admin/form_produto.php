a5d0f46739c5fa80d9cc75768b36e1c419bdb8c8
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-18 01:16:51 +0000

    deploy

--- admin/form_produto.php
+++ admin/form_produto.php
@@ -185,7 +185,7 @@ $('form').on('submit', function () {
 </script>
 
     <!-- jQuery 2.1.3 -->
-    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    
     <!-- Bootstrap 3.3.2 JS -->
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- FastClick -->
