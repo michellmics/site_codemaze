4aa927a2975b13ee5b4726ca2dacebed2db294d3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 13:46:21 +0000

    deploy

--- index.php
+++ index.php
@@ -215,7 +215,7 @@
                 <p class="lead">Conte com a Codemaze para soluções eficazes e consultoria de qualidade. Juntos, vamos transformar suas ideias em realidade.</p>
             </div><!-- end title -->
 
-            <div class="owl-services owl-theme">
+            <div class=" owl-carousel owl-theme">
                 <div class="service-widget">
                     <div class="post-media wow fadeIn">
                         <a href="uploads/gestaoMidiaSocial.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
