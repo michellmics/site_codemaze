2865be35e09f78af10016e96494c4994ae30bd1d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 00:53:53 +0000

    deploy

--- admin/form_cliente_edit.php
+++ admin/form_cliente_edit.php
@@ -11,13 +11,8 @@
   }
 */
 
-if (!isset($_POST['id'])) {
-  $id = htmlspecialchars($_POST['id']);
-  echo "ID do cliente não recebido";
-  die();
-}
 
-  $idCLient = $_POST['id'];
+  $idCLient = $_GET['id'];
   $siteAdmin = new SITE_ADMIN();
   $siteAdmin->getClientInfoById($idCLient); 
 
