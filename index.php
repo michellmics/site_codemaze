707bd6ea3ccb53275bcdb1e0d0e2d979829a10a7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 12:58:35 +0000

    deploy

--- index.php
+++ index.php
@@ -437,15 +437,6 @@
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
                     <a href="#"><img src="uploads/logo_04.png" alt="" class="img-repsonsive"></a>
                 </div>
-                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
-                    <a href="#"><img src="uploads/logo_04.png" alt="" class="img-repsonsive"></a>
-                </div>
-                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
-                    <a href="#"><img src="uploads/logo_05.png" alt="" class="img-repsonsive"></a>
-                </div>
-                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
-                    <a href="#"><img src="uploads/logo_06.png" alt="" class="img-repsonsive"></a>
-                </div>
             </div><!-- end row -->
 
         </div><!-- end container -->
