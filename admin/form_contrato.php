6d24ff2032e6c4bad2e4099bf4e5d0da664b19ce
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 23:12:21 +0000

    deploy

--- admin/form_contrato.php
+++ admin/form_contrato.php
@@ -54,7 +54,7 @@ $numeroContrato = $numeroContrato ."-".$numeroAleatorio;
          folder instead of downloading all of them to reduce the load. -->
     <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
 
