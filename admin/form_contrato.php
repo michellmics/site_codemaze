6abd6a3f850926d683bf032462511959cbf32dd4
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 23:13:15 +0000

    deploy

--- admin/form_contrato.php
+++ admin/form_contrato.php
@@ -54,7 +54,8 @@ $numeroContrato = $numeroContrato ."-".$numeroAleatorio;
          folder instead of downloading all of them to reduce the load. -->
     <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
 
-    
+        <!-- jQuery 2.1.3 -->
+    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
 
@@ -245,8 +246,7 @@ $numeroContrato = $numeroContrato ."-".$numeroAleatorio;
 <!-- Main MENU content  INI --> 
 <!-- ######################################################## -->
 
-    <!-- jQuery 2.1.3 -->
-    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
+
     <!-- Bootstrap 3.3.2 JS -->
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- FastClick -->
