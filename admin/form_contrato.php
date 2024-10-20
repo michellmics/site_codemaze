33ccf46c99685a1a742370a966ee8cc395010382
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 01:52:11 +0000

    deploy

--- admin/form_contrato.php
+++ admin/form_contrato.php
@@ -74,9 +74,9 @@
         	<!-- NOME  CPF/CNPJ RAZÃO SOCIAL-->          
 		<div style="width: 100%; margin-bottom: 20px;">
 			<div class="form-group" style="display: flex; gap: 10px; align-items: center;">
-			<div style="flex: 1;">
+			<div style="flex: 1 1 20%;">
 			<label>NÚMERO DO CONTRATO</label>
-			<input type="text" style="width: 30%; text-transform: uppercase;" pattern="[0-9]*"  class="form-control" placeholder="00000000000000" name="contrato" />
+			<input type="text" style="width: 30%; text-transform: uppercase;" pattern="[0-9]*"  class="form-control" placeholder="0000" name="contrato" />
 			</div>					      
       <div style="flex: 1;">
 			<label>NOME DO CLIENTE</label>
