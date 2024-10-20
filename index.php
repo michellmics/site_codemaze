5b1e7a57429a00867988cc6a091a6628ef968fdc
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-20 19:09:16 +0000

    deploy

--- index.php
+++ index.php
@@ -207,56 +207,7 @@
         </div><!-- end container -->
     </div><!-- end section -->
 	
-    <div id="about" class="section wb">
-        <div class="container">
-           <div class="row text-center">
-				<div class="col-md-3 col-sm-6">
-					<div class="about-item">
-						<div class="about-icon">
-							<span class="icon icon-display"></span>
-						</div>
-						<div class="about-text">
-							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6">
-					<div class="about-item">
-						<div class="about-icon">
-							<span class="icon icon-database"></span>
-						</div>
-						<div class="about-text">
-							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6">
-					<div class="about-item">
-						<div class="about-icon">
-							<span class="icon icon-magic-wand"></span>
-						</div>
-						<div class="about-text">
-							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6">
-					<div class="about-item">
-						<div class="about-icon">
-							<span class="icon icon-cloud"></span>
-						</div>
-						<div class="about-text">
-							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
-						</div>
-					</div>
-				</div>
-			</div>
-        </div><!-- end container -->
-    </div><!-- end section -->
+    
 
     <div id="services" class="parallax section lb">
         <div class="container">
@@ -492,7 +443,56 @@
         </div><!-- end container -->
     </div><!-- end section -->
 
-
+         <div id="about" class="section wb">
+        <div class="container">
+           <div class="row text-center">
+				<div class="col-md-3 col-sm-6">
+					<div class="about-item">
+						<div class="about-icon">
+							<span class="icon icon-display"></span>
+						</div>
+						<div class="about-text">
+							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="about-item">
+						<div class="about-icon">
+							<span class="icon icon-database"></span>
+						</div>
+						<div class="about-text">
+							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="about-item">
+						<div class="about-icon">
+							<span class="icon icon-magic-wand"></span>
+						</div>
+						<div class="about-text">
+							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="about-item">
+						<div class="about-icon">
+							<span class="icon icon-cloud"></span>
+						</div>
+						<div class="about-text">
+							<h3> <a href="#">Lorem ipsum dolor. </a></h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea, quis magnam deserunt. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div><!-- end container -->
+    </div><!-- end section -->
 
 
 
