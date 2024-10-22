10001433ae8a472ade6976e24ef0ca1801502c4d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-22 02:30:37 +0000

    deploy

--- admin/table_liquidacaoFinanceira.php
+++ admin/table_liquidacaoFinanceira.php
@@ -93,7 +93,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $reg
             <div class="col-xs-12">
               <div class="box">
                 <div class="box-header">
-                  <h3 class="box-title">Liquidação FInanceira</h3>
+                  <h3 class="box-title">Liquidação Financeira</h3>
                   <div class="box-tools" style="margin-bottom: 20px;">
                     
                   <div class="input-group" style="display: flex; align-items: center; gap: 10px;">
