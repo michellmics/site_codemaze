2f87bd6d32a26d4fbf271874942c8f915f0b9855
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-22 07:33:25 +0000

    deploy

--- admin/alerta_mail_vencimento.php
+++ admin/alerta_mail_vencimento.php
@@ -5,8 +5,15 @@ include_once 'objetos.php'; // Carrega a classe de conexão e objetos
 $siteAdmin = new SITE_ADMIN();
 $siteAdmin->getLiquidacaoFinanceiraInfo();
 
+/*
 echo "<pre>";
 var_dump($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA);
 echo "</pre>";
+*/
+
+foreach($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA as $array)
+{
+    echo $arra["LFI_STPAGAMENTO"];
+}
 
 ?>
