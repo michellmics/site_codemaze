9aebd477a3acaea7d52d3b7147769205b6104f8a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-22 12:29:36 +0000

    deploy

--- admin/table_liquidacaoFinanceira.php
+++ admin/table_liquidacaoFinanceira.php
@@ -1,150 +1,243 @@
 <?php
-include_once 'objetos.php'; 
-
-session_start(); 
-define('SESSION_TIMEOUT', 1800); // 30 minutos
-
-if (!isset($_SESSION['user_id'])) {
+  include_once 'objetos.php'; 
+  
+  session_start(); 
+  define('SESSION_TIMEOUT', 1800); // 30 minutos
+ 
+  if (!isset($_SESSION['user_id'])) 
+  {
     header("Location: index.php");
     exit();
-}
+  }
 
 $siteAdmin = new SITE_ADMIN();
 
-if (isset($_GET['update'])) {
-    $siteAdmin->updateLiquidacaoFinanceiraById(
-        $_GET['update'], $_GET['acao'], $_GET['dataPagamento']
-    );
+if(isset($_GET['update']))
+{
+  $siteAdmin->updateLiquidacaoFinanceiraById($_GET['update'],$_GET['acao'],$_GET['dataPagamento']);
+
 }
 
-if (isset($_GET['table_search'])) {
-    $search = $_GET['table_search'];
-    $result = $siteAdmin->getLiquidacaoFinanceiraInfoBySearch($search);
-    var_dump($result);
-} else {
-    $siteAdmin->getLiquidacaoFinanceiraInfo();
+if(isset($_GET['table_search'])) //trazer os dados de acordo com o q foi colocado na busca
+{
+  $search = $_GET['table_search'];
+  $result = $siteAdmin->getLiquidacaoFinanceiraInfoBySearch($search);
+  var_dump($result);
 }
+else
+  {
+    $siteAdmin->getLiquidacaoFinanceiraInfo();
+  }
+
 
 // Configurações de Paginação
 $registrosPorPagina = 10;
 $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 $totalRegistros = count($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA);
 $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
+
+// Determina o índice de início para a página atual
 $inicio = ($paginaAtual - 1) * $registrosPorPagina;
+
+// Divide o array para exibir apenas os registros da página atual
 $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $registrosPorPagina);
+
+//-----------------
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="UTF-8">
-    <title>Liquidação Financeira</title>
+    <title></title>
+  
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" />
-    <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" />
-</head>
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. Choose a skin from the css/skins 
+         folder instead of downloading all of them to reduce the load. -->
+    <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
 
-<body class="skin-blue">
-    <div class="content-wrapper" style="margin-left: 0; background-color: white;">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  
+<!-- ######################################################## --> 
+<!-- Main MENU content  INI --> 
+<!-- ######################################################## -->  
+  <body class="skin-blue">
+    <div class="wrapper">
+      <!-- Right side column. Contains the navbar and content of the page -->
+      <div class="content-wrapper" style="margin-left: 0px; background-color: white;">
+        <!-- Content Header (Page header) -->
         <section class="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="box">
-                        <div class="box-header">
-                            <h3 class="box-title">Liquidação Financeira</h3>
-                        </div>
-                        <div class="box-body table-responsive no-padding">
-                            <table class="table table-hover">
-                                <tr>
-                                    <th>CONTRATO</th>
-                                    <th>STATUS</th>
-                                    <th>CLIENTE</th>
-                                    <th>PRODUTO</th>
-                                    <th>PARCELA</th>
-                                    <th>VALOR</th>
-                                    <th>VENCIMENTO</th>
-                                    <th>PAGAMENTO</th>
-                                    <th>NOVA DATA</th>
-                                    <th>AÇÕES</th>
-                                </tr>
-                                <?php foreach ($dadosPagina as $liquidFin): ?>
-                                <tr>
-                                    <td><?= htmlspecialchars($liquidFin['GEC_IDGESTAO_CONTRATO']) ?></td>
-                                    <td>
-                                        <?php
-                                            $now = new DateTime();
-                                            $vencimento = new DateTime($liquidFin['LFI_DTVENCIMENTO']);
-                                            $diferenca = (int)$now->diff($vencimento)->format('%r%a');
+          <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+              <!-- general form elements -->
 
-                                            if ($diferenca < -5 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO") {
-                                                $msg = "VENCIDO";
-                                                $classIcon = "label label-danger";
-                                            } elseif ($diferenca > 0 && $diferenca < 5 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO") {
-                                                $msg = "A VENCER";
-                                                $classIcon = "label label-warning";
-                                            } elseif ($diferenca > 10 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO") {
-                                                $msg = "EM ABERTO";
-                                                $classIcon = "label label-primary";
-                                            } else {
-                                                $msg = "LIQUIDADO";
-                                                $classIcon = "label label-success";
-                                            }
-                                        ?>
-                                        <span class="<?= $classIcon ?>"><?= $msg ?></span>
-                                    </td>
-                                    <td><?= htmlspecialchars($liquidFin['CLI_NMNAME']) ?></td>
-                                    <td><?= htmlspecialchars($liquidFin['PRS_NMNOME']) ?></td>
-                                    <td><?= htmlspecialchars($liquidFin['LFI_DCNUMPARCELA']) ?></td>
-                                    <td><?= htmlspecialchars($liquidFin['LFI_DCVALOR_PARCELA']) ?></td>
-                                    <td><?= htmlspecialchars($liquidFin['LFI_DTVENCIMENTO']) ?></td>
-                                    <td><?= htmlspecialchars($liquidFin['LFI_DTPAGAMENTO']) ?></td>
-                                    <td>
-                                        <input type="text" id="datapagamento" class="form-control" placeholder="DD/MM/YYYY" />
-                                    </td>
-                                    <td>
-                                        <a href="#" onclick="return confirmarLiquidacao(<?= $liquidFin['LFI_IDLIQUIDACAOFINANCEIRA'] ?>);">
-                                            <span class="label label-info">LIQUIDAR</span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php endforeach; ?>
-                            </table>
+              <!-- INI BLOCO 1 -->
+
+
+
+
+
+
+              <div class="row">
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Liquidação Financeira</h3>
+                  <div class="box-tools" style="margin-bottom: 20px;">
+                    
+                  <div class="input-group" style="display: flex; align-items: center; gap: 10px;">
+
+
+                    <form method="GET" action="" style="display: flex;">
+                        <input 
+                            type="text" 
+                            name="table_search" 
+                            class="form-control input-sm pull-right" 
+                            style="width: 150px;" 
+                            placeholder="Buscar" 
+                            value="<?php echo isset($_GET['table_search']) ? htmlspecialchars($_GET['table_search']) : ''; ?>" 
+                        />
+                        <div class="input-group-btn">
+                            <button type="submit" class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
                         </div>
-                    </div>
-                </div>
+                    </form>
+                  </div>
+                  
+
+                  </div>
+                </div><!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                  <table class="table table-hover">
+                    <tr>
+                      <th>CONTRATO</th>
+                      <th></th>
+                      <th>CLIENTE</th>
+                      <th>PRODUTO</th>
+                      <th>PARCELA</th>
+                      <th>VALOR</th>
+                      <th>VENCIMENTO</th>
+                      <th>PAGAMENTO</th>
+                      <th></th>                    
+                    </tr>
+                    <tr>
+                    <?php foreach ($dadosPagina as $liquidFin): ?>
+                    <tr>
+                        <? 
+                          $now = new DateTime(); 
+                          $vencimento = new DateTime($liquidFin['LFI_DTVENCIMENTO']); 
+                          
+                          // Calcula a diferença em dias
+                          $diferenca = (int)$now->diff($vencimento)->format('%r%a');
+
+                          if ($diferenca < -5 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO"){$msg = "VENCIDO";$classIcon = "label label-danger";}  
+                          if ($diferenca > 0 && $diferenca < 5 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO"){$msg = "A VENCER";$classIcon = "label label-warning";}  
+                          if ($diferenca > 10 && $liquidFin['LFI_STPAGAMENTO'] != "LIQUIDADO"){$msg = "EM ABERTO";$classIcon = "label label-primary";}  
+                          if ($liquidFin['LFI_STPAGAMENTO'] == "LIQUIDADO"){$msg = "LIQUIDADO";$classIcon = "label label-success";}       
+                        ?>
+                        <td style="text-transform: uppercase; font-size: 14px;"><b><?= htmlspecialchars($liquidFin['GEC_IDGESTAO_CONTRATO']) ?></b></td>
+                        <td style="text-transform: uppercase; font-size: 15px;"><a href="#"><span class="<? echo $classIcon; ?>"><? echo $msg; ?></span></a></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['CLI_NMNAME']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['PRS_NMNOME']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['LFI_DCNUMPARCELA']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['LFI_DCVALOR_PARCELA']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['LFI_DTVENCIMENTO']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($liquidFin['LFI_DTPAGAMENTO']) ?></td> 
+                        <td>
+			                  <input type="text" style="width: 100%; text-transform: uppercase;" minlength="10" maxlength="10" class="form-control" placeholder="DD/MM/YYYY" id="datapagamento" name="datapagamento"   />
+                        </td>                   
+                        <td style="text-transform: uppercase; font-size: 15px;">
+                          <a href="#" 
+                             onclick="return confirmarLiquidacao(<?php echo $liquidFin['LFI_IDLIQUIDACAOFINANCEIRA']; ?>);">
+                             <span class="label label-info">LIQUIDAR</span>
+                          </a>
+                        </td>
+                        <td style="text-transform: uppercase; font-size: 15px;"><a href="https://www.codemaze.com.br/site/admin/table_liquidacaoFinanceira.php?update=<? echo $liquidFin['LFI_IDLIQUIDACAOFINANCEIRA']; ?>&acao=ABERTO" target="_self" onclick="return confirmacao();"><span class="label label-default">DEIXAR ABERTO</span></a></td>
+                                           
+                      </tr>
+                    <?php endforeach; ?>   
+                    </tr>
+                  </table>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
             </div>
+          </div>
 
-            <nav aria-label="Page navigation" class="text-center">
-                <ul class="pagination">
-                    <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
+<!-- Paginação -->
+<nav aria-label="Page navigation" class="text-center">
+            <ul class="pagination">
+                <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
                     <li class="<?= ($i == $paginaAtual) ? 'active' : '' ?>">
                         <a href="?pagina=<?= $i ?>"><?= $i ?></a>
                     </li>
-                    <?php endfor; ?>
-                </ul>
-            </nav>
-        </section>
-    </div>
+                <?php endfor; ?>
+            </ul>
+        </nav>
 
+
+
+
+              <!-- FIM BLOCO 1 -->
+
+              </div><!-- /.box-body -->
+            </div><!-- /.box -->
+        </div><!--/.col (right) -->
+      </div>   <!-- /.row -->
+    </section><!-- /.content -->
+
+<!-- ######################################################## --> 
+<!-- Main MENU content  INI --> 
+<!-- ######################################################## -->
+
+    <!-- jQuery 2.1.3 -->
     <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script>
-        function confirmarLiquidacao(id) {
-            const dataPagamento = document.getElementById('datapagamento').value;
-            if (!dataPagamento) {
-                alert("Por favor, insira a data de pagamento.");
-                return false;
-            }
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- FastClick -->
+    <script src='plugins/fastclick/fastclick.min.js'></script>
 
-            if (confirm("Tem certeza que deseja liquidar o pagamento?")) {
-                const url = `https://www.codemaze.com.br/site/admin/table_liquidacaoFinanceira.php?update=${id}&acao=LIQUIDADO&dataPagamento=${encodeURIComponent(dataPagamento)}`;
-                window.location.href = url;
-                return true;
-            }
+    <script>
+      function confirmacao() 
+      {
+        return confirm("Tem certeza que deseja mudar o status do pagamento");
+      }
+
+      function confirmarLiquidacao(id) 
+      {
+        const datapagamento = document.getElementById('datapagamento').value;
+
+        if (!datapagamento) {
+            alert("Por favor, insira a data de pagamento.");
             return false;
         }
-    </script>
-</body>
+      
+        if (confirm("Tem certeza que deseja liquidar o pagamento?")) {
+            const url = `https://www.codemaze.com.br/site/admin/table_liquidacaoFinanceira.php?update=${id}&acao=LIQUIDADO&dataPagamento=${encodeURIComponent(dataPagamento)}`;
+            window.location.href = url; // Redireciona para a URL construída
+            return true;
+        }
+          return false; // Cancela a ação se não confirmado
+        }
+    </script>              
+    
+
+
+  </body>
 </html>
