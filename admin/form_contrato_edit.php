53c017d2da0cea30c290f6f4093e2a81cf07c496
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-22 04:13:05 +0000

    deploy

--- admin/form_contrato_edit.php
+++ admin/form_contrato_edit.php
@@ -287,10 +287,7 @@ $siteAdmin->ARRAY_CONTRATOINFO[0]["GEC_DTVENCIMENTO"] = convertDate($siteAdmin->
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- FastClick -->
     <script src='plugins/fastclick/fastclick.min.js'></script>
-    <!-- AdminLTE App -->
-    <script src="dist/js/app.min.js" type="text/javascript"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js" type="text/javascript"></script>
+ 
 
   <script>
     $(document).ready(function() {
