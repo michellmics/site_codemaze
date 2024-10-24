7ed4265ccfdaf5b318cf9355f129eb31d583cf88
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-24 00:37:16 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -114,10 +114,6 @@
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- FastClick -->
     <script src='plugins/fastclick/fastclick.min.js'></script>
-    <!-- AdminLTE App -->
-    <script src="dist/js/app.min.js" type="text/javascript"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js" type="text/javascript"></script>
     <!-- FLOT CHARTS -->
     <script src="plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
     <!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
