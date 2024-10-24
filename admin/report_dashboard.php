d45b69e1f9dc773c0d51a7fc836c43ea111e6923
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-24 00:33:44 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -10,11 +10,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-    <!-- Theme style -->
-    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-    <!-- AdminLTE Skins. Choose a skin from the css/skins 
-         folder instead of downloading all of them to reduce the load. -->
-    <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -31,17 +27,7 @@
 
       <!-- Right side column. Contains the navbar and content of the page -->
       <div class="content-wrapper" style="margin-left: 0px; background-color: white;>
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Flot Charts
-            <small>preview sample</small>
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Blank page</li>
-          </ol>
-        </section>
+
 
         <!-- Main content -->
         <section class="content">
