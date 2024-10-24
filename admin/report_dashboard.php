f1d9873618e8414ef46d933c83dcda7a50af27cc
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-24 00:21:46 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -130,23 +130,23 @@
     </div><!-- ./wrapper -->
 
     <!-- jQuery 2.1.3 -->
-    <script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
+    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
-    <script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- FastClick -->
-    <script src='../../plugins/fastclick/fastclick.min.js'></script>
+    <script src='plugins/fastclick/fastclick.min.js'></script>
     <!-- AdminLTE App -->
-    <script src="../../dist/js/app.min.js" type="text/javascript"></script>
+    <script src="dist/js/app.min.js" type="text/javascript"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js" type="text/javascript"></script>
+    <script src="dist/js/demo.js" type="text/javascript"></script>
     <!-- FLOT CHARTS -->
-    <script src="../../plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+    <script src="plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
     <!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-    <script src="../../plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+    <script src="plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
     <!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-    <script src="../../plugins/flot/jquery.flot.pie.min.js" type="text/javascript"></script>
+    <script src="plugins/flot/jquery.flot.pie.min.js" type="text/javascript"></script>
     <!-- FLOT CATEGORIES PLUGIN - Used to draw bar charts -->
-    <script src="../../plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+    <script src="plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
 
     <!-- Page script -->
     <script type="text/javascript">
