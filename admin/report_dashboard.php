8b75f810c15b81bbcc92dea16401fe718f9af8fb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-24 00:34:24 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -106,14 +106,7 @@
           </div><!-- /.row -->
         </section><!-- /.content -->
 
-      </div><!-- /.content-wrapper -->
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 2.0
-        </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
-      </footer>
-    </div><!-- ./wrapper -->
+>
 
     <!-- jQuery 2.1.3 -->
     <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
