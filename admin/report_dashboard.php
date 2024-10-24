27f77ed7f2a2222e6a4aff317269384a5ead310b
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-24 01:10:47 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -33,7 +33,24 @@
         <section class="content">
           <div class="row">
             <div class="col-md-6">
- Aquiiiii
+            <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-6 text-center">
+                      <input type="text" class="knob" value="30" data-width="90" data-height="90" data-fgColor="#3c8dbc"/>
+                      <div class="knob-label">New Visitors</div>
+                    </div><!-- ./col -->
+                    <div class="col-md-3 col-sm-6 col-xs-6 text-center">
+                      <input type="text" class="knob" value="70" data-width="90" data-height="90" data-fgColor="#f56954"/>
+                      <div class="knob-label">Bounce Rate</div>
+                    </div><!-- ./col -->
+                    <div class="col-md-3 col-sm-6 col-xs-6 text-center">
+                      <input type="text" class="knob" value="-80" data-min="-150" data-max="150" data-width="90" data-height="90" data-fgColor="#00a65a"/>
+                      <div class="knob-label">Server Load</div>
+                    </div><!-- ./col -->
+                    <div class="col-md-3 col-sm-6 col-xs-6 text-center">
+                      <input type="text" class="knob" value="40" data-width="90" data-height="90" data-fgColor="#00c0ef"/>
+                      <div class="knob-label">Disk Space</div>
+                    </div><!-- ./col -->
+                  </div><!-- /.row -->
             </div>
           </div><!-- /.row -->
         </section><!-- /.content -->
