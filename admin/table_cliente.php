1d63c6ab84615d75133d8cbb2bbac27e10596fc7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-27 13:22:00 +0000

    deploy

--- admin/table_cliente.php
+++ admin/table_cliente.php
@@ -136,7 +136,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_CLIENTINFO, $inicio, $registrosPorP
                    <tr>
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($client['CLI_IDCLIENT']) ?></td>
                         <td style="text-transform: uppercase; font-size: 15px;">
-                            <a href="https://www.codemaze.com.br/site/admin/form_contrato_edit.php?id=<? echo $liquidFin['GEC_IDGESTAO_CONTRATO']; ?>" target="_self"><span class="label <?= $client['CLI_STSTATUSPENDING'] == 'Vencido' ? 'label-danger' : 'label-success' ?>">
+                            <a href="https://www.codemaze.com.br/site/admin/table_liquidacaoFinanceira.php?table_search=<? echo $client['CLI_NMNAME']; ?>" target="_self"><span class="label <?= $client['CLI_STSTATUSPENDING'] == 'Vencido' ? 'label-danger' : 'label-success' ?>">
                                 <?= htmlspecialchars($client['CLI_STSTATUSPENDING']) ?> </a>
                             </span>
                         </td> 
