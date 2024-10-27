d8dd415b4ee863144cc0129a4df24e28789af37e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-27 23:53:33 +0000

    deploy

--- admin/table_liquidacaoFinanceira.php
+++ admin/table_liquidacaoFinanceira.php
@@ -162,7 +162,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $reg
                           
                           $idOrdemPagamento = htmlspecialchars($liquidFin['LFI_IDOP']);
                           $linkBoleto = $liquidFin['LFI_PAGSEGURO_LINK_BOLETO'];
-                          $boleto = $liquidFin['LFI_PAGSEGURO_LINK_BOLETO'] == NULL ? "<a href='https://www.codemaze.com.br/site/admin/boleto_proc.php?LFI_IDOP=$idOrdemPagamento' target='_self'>Gerar Boleto</a>" : "<a href='$linkBoleto' target='_blank'><i style='width: 50px; height: 50px;' class='fas fa-file-pdf'></i></a>";
+                          $boleto = $liquidFin['LFI_PAGSEGURO_LINK_BOLETO'] == NULL ? "<a href='https://www.codemaze.com.br/site/admin/boleto_proc.php?LFI_IDOP=$idOrdemPagamento' target='_self'>Gerar Boleto</a>" : "<a href='$linkBoleto' target='_blank'><i class='fas fa-file-pdf'></i></a>";
 
                         ?> 
                         <td style="text-transform: uppercase; font-size: 14px; color: red !important; vertical-align: middle;"><b><a href="https://www.codemaze.com.br/site/admin/form_contrato_edit.php?id=<? echo $liquidFin['GEC_IDGESTAO_CONTRATO']; ?>" target="_self"><?= htmlspecialchars($liquidFin['GEC_IDGESTAO_CONTRATO']) ?></a></b></td>
