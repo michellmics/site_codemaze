0bd36797686d690389a2f89867d75686d10a5506
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-28 00:45:28 +0000

    deploy

--- admin/boleto_proc.php
+++ admin/boleto_proc.php
@@ -1,13 +1,6 @@
 <?php
 
-session_start(); 
-define('SESSION_TIMEOUT', 1800); // 30 minutos
 
-if (!isset($_SESSION['user_id'])) 
-{
-  header("Location: index.php");
-  exit();
-}
 
     include_once 'objetos.php';
 
