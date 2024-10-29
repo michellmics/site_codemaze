7627903b1ae013895ec712fd5aae0ccd9d4ad310
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-29 03:08:56 +0000

    deploy

--- admin/cron_fechamento_balanco.php
+++ admin/cron_fechamento_balanco.php
@@ -10,5 +10,28 @@ $liquidoMêsCorrente = $countReceitaMesCorrente["0"]["TOTAL"] - $despesas;
 
 $dashboardValues->insertBalancoMes($countReceitaMesCorrente["0"]["TOTAL"], $despesas, $liquidoMêsCorrente);
 
+
+$emalCobrança = "suporte@codemaze.com.br"; 
+
+$subject = "FECHAMENTO FINANCEIRO MENSAL";
+$msg = "
+        Olá <b>Admins</b>, bom dia! <br><br>
+        O sistema Intranet Codemaze acaba de fazer o fechamento financeiro mensal. <br><br>
+        
+        Atenciosamente,<br><br>
+
+        Sistema Intranet Codemaze <br><br>
+        <img src='https://www.codemaze.com.br/site/images/logos/logo.jpg' alt='Codemaze Logo' style='max-width:200px;'> <br>
+
+        Codemaze - Soluções de MKT e Software<br><br>
+        faleconosco@codemaze.com.br<br>
+        suporte@codemaze.com.br<br>
+        <a href='https://www.codemaze.com.br'>codemaze.com.br</a><br>
+        ";
+
+
+$siteAdmin->notifyPendenciasEmail($subject, $msg, $emalCobrança); 
+
+
 echo "Fim do Script";
 ?>
