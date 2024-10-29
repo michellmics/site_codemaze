ff7701bac5d5330a1cec1b26abf5f663c4a1eb66
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-29 02:52:18 +0000

    deploy

--- admin/cron_fechamento_balanco.php
+++ admin/cron_fechamento_balanco.php
@@ -8,9 +8,7 @@ $despesas = 125; //provisorio
 $liquidoMêsCorrente = $countReceitaMesCorrente["0"]["TOTAL"] - $despesas;
 
 
-$dashboardValues->insertBalancoMes($countReceitaMesCorrente, $despesas, $liquidoMêsCorrente);
-
-var_dump($liquidoMêsCorrente);
+$dashboardValues->insertBalancoMes($countReceitaMesCorrente["0"]["TOTAL"], $despesas, $liquidoMêsCorrente);
 
 echo "Fim do Script";
 ?>
