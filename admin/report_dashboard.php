3fb262490ad1fb2134eb304be1b802ad5436c9b3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-29 00:56:06 +0000

    deploy

--- admin/report_dashboard.php
+++ admin/report_dashboard.php
@@ -104,6 +104,63 @@ $despesas = 125; //provisorio
                 </a>
               </div>
             </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-red">
+                <div class="inner">
+                  <h3>
+                    65
+                  </h3>
+                  <p>
+                    Unique Visitors
+                  </p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                  More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3>
+                    53<sup style="font-size: 20px">%</sup>
+                  </h3>
+                  <p>
+                    Bounce Rate
+                  </p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                  More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-purple">
+                <div class="inner">
+                  <h3>
+                    80<sup style="font-size: 20px">%</sup>
+                  </h3>
+                  <p>
+                    Conversion Rate
+                  </p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-ios-briefcase-outline"></i>
+                </div>
+                <a href="#" class="small-box-footer">
+                  More info <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
                     <div class="col-md-3 col-sm-6 col-xs-6 text-center">
                       <input type="text" class="knob" value="<? echo $totalCLient["0"]["TOTAL"]; ?>" data-max="100" data-width="90" data-height="90" data-fgColor="#3c8dbc"/>
                       <div class="knob-label">Clientes</div>
