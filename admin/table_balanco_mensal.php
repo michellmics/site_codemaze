b353e2c57b1bd7c2279e6cf85dc2b993ffa43626
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-10-31 01:30:12 +0000

    deploy

--- admin/table_balanco_mensal.php
+++ admin/table_balanco_mensal.php
@@ -107,7 +107,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_BALANCOMENSAL, $inicio, $registrosP
                         ?>
 
                         <td style="text-transform: uppercase; font-size: 12px;"><?= $dataFormatada ?></td>
-                        <td style="text-transform: uppercase; font-size: 12px; color: yellow;">R$<?= htmlspecialchars($balanco['BLM_DCRECEITA']) ?></td>
+                        <td style="text-transform: uppercase; font-size: 12px; color: blue;">R$<?= htmlspecialchars($balanco['BLM_DCRECEITA']) ?></td>
                         <td style="text-transform: uppercase; font-size: 12px; color: red;">R$<?= htmlspecialchars($balanco['BLM_DCDESPESA']) ?></td>
                         <td style="text-transform: uppercase; font-size: 12px; color: green;">R$<?= htmlspecialchars($balanco['BLM_DCLIQUIDO']) ?></td>                 
                       </tr>
