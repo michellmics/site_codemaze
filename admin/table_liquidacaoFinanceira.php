60f89b76e120eacfc42e9763f66c794fb151c24e
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-02 02:52:36 +0000

    deploy

--- admin/table_liquidacaoFinanceira.php
+++ admin/table_liquidacaoFinanceira.php
@@ -240,6 +240,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $reg
         </div><!--/.col (right) -->
       </div>   <!-- /.row -->
     </section><!-- /.content -->
+    
 
 <!-- ######################################################## --> 
 <!-- Main MENU content  INI --> 
@@ -260,7 +261,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $reg
 
       function showErrorModal(message) {
           document.getElementById('modalBodyContent').innerText = message; // Define a mensagem de erro
-          $('#modalBodyContent').modal('show'); // Abre o modal
+          $('#alertModal').modal('show'); // Abre o modal
       }
 
       function confirmarLiquidacao(id) {
@@ -291,10 +292,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_LIQUIDACAOFINANCEIRA, $inicio, $reg
 
     </script>              
     
-    <!-- jQuery 2.1.3 -->
-    <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
-    <!-- Bootstrap 3.3.2 JS -->
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
 
   </body>
 </html>
