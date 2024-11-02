afa115cb61422ead4cf4a0988be25496fbc67b89
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-02 03:05:27 +0000

    deploy

--- admin/client_proc_edit.php
+++ admin/client_proc_edit.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'objetos.php'; // Carrega a classe de conexão e objetos
-include 'modal.php';
+include 'modal_ok.php';
 
 session_start(); // Inicia a sessão para armazenar dados do usuário
 
@@ -53,16 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
   </head>
   <body class="skin-blue">
 
-  <script>
-        function showModal(message) {
-          // Define o conteúdo do corpo do modal
-          document.getElementById('modalBodyContent').innerText = message;
-          // Abre o modal
-          $('#alertModal').modal('show');
-        }
-    </script>
-
-
     <!-- jQuery 2.1.3 -->
     <script src="plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
@@ -73,5 +63,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     <script src="dist/js/app.min.js" type="text/javascript"></script>
     <!-- AdminLTE for demo purposes -->
     <script src="dist/js/demo.js" type="text/javascript"></script>
+
+    <script>
+        function showModal(message) {
+          // Define o conteúdo do corpo do modal
+          document.getElementById('modalBodyContent').innerText = message;
+          // Abre o modal
+          $('#alertModal').modal('show');
+        }
+    </script>
+
   </body>
 </html>
