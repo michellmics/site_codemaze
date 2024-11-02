b2b92a9ede4f2c9fd2757fae27bcccd5126fd400
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-02 00:52:04 +0000

    deploy

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -210,7 +210,7 @@
 
                           echo "<li style='display: flex; align-items: center; justify-content: space-between; padding: 5px 0;'>
                                   <a href='#'><i class='".$icon."'></i>$msg</a>
-                                   <button style='padding: 3px 6px; border: none; background-color: #007bff; color: white; border-radius: 4px; cursor: pointer;'>OK</button>  
+                                   <button style='padding: 3px 6px; border: none; background-color: #007bff; color: white; border-radius: 4px; cursor: pointer; margin-right: 3px;'>OK</button>  
                                 </li>";
                         }
                       
