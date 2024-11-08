2bc1f981e3d53bc7f951ecaad40eeb53ede3d9f2
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-08 00:20:01 -0300

    Update intranet.php

--- admin/intranet.php
+++ admin/intranet.php
@@ -127,7 +127,7 @@
             SGCS
         </a>
         <a href="https://accounts.mlabs.io/accounts/sign_in" target="_blank">
-            <i class="fas fa-info-circle"></i>
+            <i class="fa-instagram"></i>
             mLabs
         </a>
         <a href="https://cpanel.codemaze.com.br/unprotected/redirect.html?goto_uri=" target="_blank">
