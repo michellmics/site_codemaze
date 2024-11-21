c7e9ebcf7c7a59650cc0cbe5c471b38eae74f5eb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-21 01:55:28 +0000

    deploy

--- admin/cron_envio_email_boleto.php
+++ admin/cron_envio_email_boleto.php
@@ -65,8 +65,6 @@ foreach($LISTA_EMAIL_BOLETOS as $itens)
         $aux++;
     }
 
-    die(); 
-    
     $nome = $boletos["CLI_NMNAME"];
     $assunto = "Codemaze - Fatura a vencer";
     $email = $boletos["GEC_DCEMAILCOBRANCA"];
