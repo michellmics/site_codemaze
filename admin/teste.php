3954bf97b43496cfce78ae25e4d271a79f07a6fc
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-22 02:33:57 +0000

    deploy

--- admin/teste.php
+++ admin/teste.php
@@ -23,7 +23,7 @@
         <script>
             Swal.fire({
   title: 'Are you sure?',
-  text: 'You won't be able to revert this!',
+  text: 'You wont be able to revert this!',
   icon: 'warning',
   showCancelButton: true,
   confirmButtonColor: '#3085d6',
