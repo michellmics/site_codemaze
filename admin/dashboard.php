e716d7c8baa4dc324760e3a9f74f7a433eae12a3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-25 03:44:13 +0000

    deploy

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -279,6 +279,16 @@ if (isset($_GET['alerta'])) //reconheciento de alerta
          
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
+          <div class="user-panel">
+            <div class="pull-left image">
+              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+              <p>Alexander Pierce</p>
+
+              <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+          </div>
             <li class="header"><?php echo htmlspecialchars($siteAdmin->ARRAY_SITEINFO["SBI_DCDOMAINSITE"]); ?></li>
             <li class="treeview">
               <a href="#">
