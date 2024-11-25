a44ccc5eede2ec41eceb228c86af8e0b1ff8266d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-25 04:00:19 +0000

    deploy

--- admin/table_cliente.php
+++ admin/table_cliente.php
@@ -95,10 +95,8 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_CLIENTINFO, $inicio, $registrosPorP
                   <div class="box-tools" style="margin-bottom: 20px;">
                   
                   <div class="input-group" style="display: flex; align-items: center; gap: 10px;">
-                  <div style="margin-right: auto;"> 
-              <input type="checkbox" id="status" name="status" value="Ativo"> Ativo
-            </div>
-
+                  <input type="checkbox" id="status" name="status" value="Ativo"> Ativo     | 
+                  
                    <!-- Botão "Adicionar Produto" -->
                    <button class="btn btn-block btn-info btn-sm" onclick="window.location.href='form_cliente.php';">ADICIONAR CLIENTE</button>
                     <form method="GET" action="" style="display: flex;">
