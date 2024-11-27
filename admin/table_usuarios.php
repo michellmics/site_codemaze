c0beb3852603154cd68d4b7a3051ae01cdf209ce
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-27 02:40:01 +0000

    deploy

--- admin/table_usuarios.php
+++ admin/table_usuarios.php
@@ -140,7 +140,6 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_USERINFO, $inicio, $registrosPorPag
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($usuario['USA_DCEMAIL']) ?></td>                        
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($usuario['USA_DCSEXO']) ?></td>
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($usuario['USA_DCNIVELDEACESSO']) ?></td>                    
-                        <td style="text-transform: uppercase; font-size: 15px;"><a href="https://www.codemaze.com.br/site/admin/form_produto_edit.php?id=<? echo $usuario['PRS_IDPRODUTO_SERVICO']; ?>" target="_self"><span class="label label-warning">EDITAR</span></a></td>
                       </tr>
                     <?php endforeach; ?>   
                     </tr>
