4b97853ae99d4679d1536747e791efbb5ae0fae0
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-27 03:12:45 +0000

    deploy

--- admin/table_usuarios.php
+++ admin/table_usuarios.php
@@ -133,7 +133,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_USERINFO, $inicio, $registrosPorPag
                     </tr>
                     <tr>
                       <?php foreach ($dadosPagina as $admusers): ?>
-                        <tr>
+                        <tr> <? var_dump($admusers); ?>
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($admusers['USA_IDUSERADMIN']) ?></td> 
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($admusers['USA_DCNOME']) ?></td>
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($admusers['USA_DCEMAIL']) ?></td>                       
