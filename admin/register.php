f3ddcceacca1f24c27f3f53567cef40ff932d733
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-28 01:46:08 +0000

    deploy

--- admin/register.php
+++ admin/register.php
@@ -139,7 +139,7 @@ $result = $siteAdmin->getSiteInfo();
     <!-- SWEETALERT 2 -->   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-      function confirmDelete(userId){
+      function confirmAndSubmit(event) {
         event.preventDefault(); // Impede o envio padrão do formulário
         Swal.fire({
           title: 'Formulário de usuários',
