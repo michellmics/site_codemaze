23e596152a1599ef4ac064fb5186369c720b09d6
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-30 01:36:54 +0000

    deploy

--- admin/agenda.php
+++ admin/agenda.php
@@ -4,7 +4,6 @@
 <meta charset='utf-8' />
 <script src='dist/js/index.global.js'></script>
 <script>
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
@@ -16,8 +15,6 @@
         right: 'listDay,listWeek'
       },
 
-      // customize the button names,
-      // otherwise they'd all just say "list"
       views: {
         listDay: { buttonText: 'Listar por dia' },
         listWeek: { buttonText: 'Listar por semana' }
@@ -25,74 +22,52 @@
 
       initialView: 'listWeek',
       initialDate: '2023-01-12',
-      navLinks: true, // can click day/week names to navigate views
+      navLinks: true,
       editable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
+      dayMaxEvents: true,
       events: [
         {
           title: 'All Day Event',
-          start: '2023-01-01'
+          start: '2023-01-01',
+          responsavel: 'João'
         },
         {
           title: 'Long Event',
           start: '2023-01-07',
           end: '2023-01-10',
-          responsavel: 'João'
+          responsavel: 'Maria'
         },
         {
           groupId: 999,
           title: 'Repeating Event',
-          start: '2023-01-09T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2023-01-16T16:00:00'
+          start: '2023-01-09T16:00:00',
+          responsavel: 'Carlos'
         },
         {
           title: 'Conference',
           start: '2023-01-11',
-          end: '2023-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T10:30:00',
-          end: '2023-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2023-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2023-01-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2023-01-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2023-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2023-01-28'
+          end: '2023-01-13',
+          responsavel: 'Ana'
         }
-      ]
+      ],
+
+      eventContent: function(info) {
+        return {
+          html: `
+            <div>
+              <strong>${info.event.title}</strong><br>
+              <em>Responsável: ${info.event.extendedProps.responsavel || 'Não definido'}</em>
+            </div>
+          `
+        };
+      }
+
     });
 
     calendar.render();
   });
-
 </script>
 <style>
-
   body {
     margin: 40px 10px;
     padding: 0;
@@ -104,12 +79,9 @@
     max-width: 1100px;
     margin: 0 auto;
   }
-
 </style>
 </head>
 <body>
-
   <div id='calendar'></div>
-
 </body>
 </html>
