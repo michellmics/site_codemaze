84ddf758cf85b5c48b661b322487a8e14e1a9453
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-11-30 03:36:57 +0000

    deploy

--- admin/form_agenda_edit.php
+++ admin/form_agenda_edit.php
@@ -102,7 +102,7 @@
 			<div style="flex: 1;">
 			<label>FUNCIONÁRIO</label>
 			<select required class="form-control" name="funcionario" style="width: 100%;">
-      <option value="<? echo $siteAdmin->ARRAY_USERINFO[0]["USA_IDUSERADMIN"]; ?>" selected><? echo $siteAdmin->ARRAY_USERINFO[0]["USA_DCNOME"]; ?></option>
+      <option value="<?php echo $siteAdmin->ARRAY_USERINFO[0]["USA_IDUSERADMIN"]; ?>" selected><?php echo $siteAdmin->ARRAY_USERINFO[0]["USA_DCNOME"]; ?></option>
 
 			</select> 
 			</div> 
