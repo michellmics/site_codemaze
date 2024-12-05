8bf3d3b600f46acd2d11fc1161aaa220146af4a3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-05 01:08:32 +0000

    deploy

--- admin/form_agenda_edit.php
+++ admin/form_agenda_edit.php
@@ -369,7 +369,6 @@ $('form').on('submit', function () {
     <script src='plugins/fastclick/fastclick.min.js'></script>
     <!-- AdminLTE App -->
     <script src="dist/js/app.min.js" type="text/javascript"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js" type="text/javascript"></script>
+
   </body>
 </html>
