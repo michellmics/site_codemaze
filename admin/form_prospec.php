dac9898344ae1340e5e0a728ea15d1a3958144b0
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 02:09:44 +0000

    deploy

--- admin/form_prospec.php
+++ admin/form_prospec.php
@@ -83,7 +83,7 @@
                       <div class="input-group-addon">
                         <i class="fa fa-phone"></i>
                       </div>
-                      <input id="telefone" name="telefone" type="text" class="form-control" data-inputmask='"mask": "(999) 999-9999"' data-mask/>
+                      <input id="telefone" name="telefone" type="text" class="form-control" data-inputmask='"mask": "(99) 9999-9999"' data-mask/>
                     </div><!-- /.input group -->
                   </div><!-- /.form group -->
                     <div class="form-group">
