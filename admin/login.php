d112f6a668b10052d8d3086636ee87660ab40532
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 18:54:56 +0000

    deploy

--- admin/login.php
+++ admin/login.php
@@ -43,7 +43,7 @@ class LoginSystem extends SITE_ADMIN
                     }
                     else
                         {
-                            //echo '<meta http-equiv="refresh" content="0;url=table_prospec.php">'; // Redireciona após login bem-sucedido
+                            
                             header("Location: table_prospec.php");
                             exit(); 
                         }
