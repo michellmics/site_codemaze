5dfc6229ac880fda56a96c192cac0f3ed4fd19a5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 19:05:38 +0000

    deploy

--- admin/login.php
+++ admin/login.php
@@ -36,21 +36,18 @@ class LoginSystem extends SITE_ADMIN
 
                 if($area == "Intranet")
                 {
-                    //echo '<meta http-equiv="refresh" content="0;url=intranet.php">'; // Redireciona após login bem-sucedido
-                    header("Location: intranet.php");
+                    echo '<meta http-equiv="refresh" content="0;url=intranet.php">'; // Redireciona após login bem-sucedido
                     exit();
                 }
                 else
                     if($user['USA_STPROSPEC'] != "SIM")
                     {
-                        //echo '<meta http-equiv="refresh" content="0;url=noAuth.html">'; // Redireciona após login bem-sucedido
-                        header("Location: noAuth.html");
+                        echo '<meta http-equiv="refresh" content="0;url=noAuth.html">'; // Redireciona após login bem-sucedido
                         exit();
                     }
                     else
                         {
-                            //echo '<meta http-equiv="refresh" content="0;url=table_prospec.php">'; // Redireciona após login bem-sucedido
-                            header("Location: table_prospec.php");
+                            echo '<meta http-equiv="refresh" content="0;url=table_prospec.php">'; // Redireciona após login bem-sucedido
                             exit(); 
                         }
             } else 
