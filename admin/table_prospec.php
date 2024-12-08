67c9648cd826758f701adde7d0c9b3b9f922f65a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 13:52:56 +0000

    deploy

--- admin/table_prospec.php
+++ admin/table_prospec.php
@@ -117,7 +117,7 @@ $dadosPagina = array_slice($siteAdmin->ARRAY_PROSPEC_CLIENTESINFO, $inicio, $reg
             <div class="col-xs-12">
               <div class="box">
                 <div class="box-header">
-                  <h3 class="box-title">Lista de Prospecção - <? echo $siteAdmin->ARRAY_USERINFO["USA_DCNOME"]; ?></h3>
+                  <h3 class="box-title"><b>Lista de Prospecção</b> :  <? echo $siteAdmin->ARRAY_USERINFO["USA_DCNOME"]; ?></h3>
                   <div class="box-tools" style="margin-bottom: 20px;">
                   
                   <div class="input-group" style="display: flex; align-items: center; gap: 10px;">
