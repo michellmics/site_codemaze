87923215ba16444cf853b28fe724065c1532c6e1
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 00:26:41 +0000

    deploy

---
+++ admin/form_prospec.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. Choose a skin from the css/skins 
+         folder instead of downloading all of them to reduce the load. -->
+    <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+      .main-sidebar {
+        display: none;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="content">
+      <section class="content">
+        <div class="error-page">
+          <h2 class="headline text-yellow"> 403</h2>
+          <div class="error-content">
+            <h3><i class="fa fa-warning text-yellow"></i> Permissões insuficientes.</h3>
+            <p>
+              Suas credenciais não autorizam o acesso a esta seção.
+              Por favor, entre em contato com o administrador do sistema para verificar suas permissões e tente novamente.
+            </p>
+          </div><!-- /.error-content -->
+        </div><!-- /.error-page -->
+      </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+  </body>
+</html>
