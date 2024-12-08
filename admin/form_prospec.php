a2aa0894b6fba35f13fe768a5db0da91162aedad
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 00:48:56 +0000

    deploy

--- admin/form_prospec.php
+++ admin/form_prospec.php
@@ -48,63 +48,22 @@
                 <div class="box-body">
                   <form role="form">
                     <!-- text input -->
-                    <div class="form-group">
-                      <label>Text</label>
-                      <input type="text" class="form-control" placeholder="Enter ..."/>
-                    </div>
-                    <div class="form-group">
-                      <label>Text Disabled</label>
-                      <input type="text" class="form-control" placeholder="Enter ..." disabled/>
-                    </div>
-
-                    <!-- textarea -->
-                    <div class="form-group">
-                      <label>Textarea</label>
-                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                    </div>
-                    <div class="form-group">
-                      <label>Textarea Disabled</label>
-                      <textarea class="form-control" rows="3" placeholder="Enter ..." disabled></textarea>
-                    </div>
-
-                    <!-- input states -->
-                    <div class="form-group has-success">
-                      <label class="control-label" for="inputSuccess"><i class="fa fa-check"></i> Input with success</label>
-                      <input type="text" class="form-control" id="inputSuccess" placeholder="Enter ..."/>
-                    </div>
                     <div class="form-group has-warning">
-                      <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Input with warning</label>
+                      <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Nome do CLiente</label>
                       <input type="text" class="form-control" id="inputWarning" placeholder="Enter ..."/>
                     </div>
-                    <div class="form-group has-error">
-                      <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Input with error</label>
-                      <input type="text" class="form-control" id="inputError" placeholder="Enter ..."/>
-                    </div>
-
-                    <!-- checkbox -->
                     <div class="form-group">
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox"/>
-                          Checkbox 1
-                        </label>
-                      </div>
-
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox"/>
-                          Checkbox 2
-                        </label>
-                      </div>
-
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox" disabled/>
-                          Checkbox disabled
-                        </label>
-                      </div>
+                      <label>Endereço:</label>
+                      <input type="text" class="form-control" placeholder="Enter ..." />
                     </div>
-
+                    <div class="form-group">
+                      <label>Nome do Contato:</label>
+                      <input type="text" class="form-control" placeholder="Enter ..." />
+                    </div>
+                    <div class="form-group">
+                      <label>E-mail:</label>
+                      <input type="text" class="form-control" placeholder="Enter ..." />
+                    </div>
                     <!-- radio -->
                     <div class="form-group">
                       <div class="radio">
@@ -126,50 +85,12 @@
                         </label>
                       </div>
                     </div>
-
-                    <!-- select -->
+                    <!-- textarea -->
                     <div class="form-group">
-                      <label>Select</label>
-                      <select class="form-control">
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
+                      <label>Observações</label>
+                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
                     </div>
-                    <div class="form-group">
-                      <label>Select Disabled</label>
-                      <select class="form-control" disabled>
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
-
-                    <!-- Select multiple-->
-                    <div class="form-group">
-                      <label>Select Multiple</label>
-                      <select multiple class="form-control">
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label>Select Multiple Disabled</label>
-                      <select multiple class="form-control" disabled>
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
+                   
 
                   </form>
                 </div><!-- /.box-body -->
