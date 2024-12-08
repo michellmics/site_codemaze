539bce28ca32b0117665563c699a5d2d63b121ee
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 15:32:20 +0000

    deploy

--- admin/table_prospec.php
+++ admin/table_prospec.php
@@ -138,8 +138,8 @@ else
                     <tr>
                       <th></th> 
                       <th>NOME</th>
-                      <th>MAPS</th>
-                      <th>STATUS</th>                     
+                      <th>STATUS</th>  
+                      <th>MAPS</th>                   
                     </tr>
                     <tr>
                     <? 
@@ -183,8 +183,9 @@ else
                             </span>
                         </td> 
                         <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($client['PRC_NMNOME']) ?></td>
-                        <td style="text-transform: uppercase; font-size: 12px;"><a href="<?php echo $linkMaps; ?>" target="_blank"><span <? echo $classLabel; ?>><i class="fa fa-location-arrow"></i></span></a></td>
-                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($client['PRC_STSTATUS']) ?></td>                    
+                        
+                        <td style="text-transform: uppercase; font-size: 12px;"><?= htmlspecialchars($client['PRC_STSTATUS']) ?></td>  
+                        <td style="text-transform: uppercase; font-size: 12px;"><a href="<?php echo $linkMaps; ?>" target="_blank"><span <? echo $classLabel; ?>><i class="fa fa-location-arrow"></i></span></a></td>                  
                         <td style="text-transform: uppercase; font-size: 15px;"><a href="https://www.codemaze.com.br/site/admin/form_prospec_edit.php?id=<? echo $client['PRC_IDPROSPEC_CLIENTES']; ?>" target="_self"><span class="label label-warning"><i class="fa fa-eye"></i></span></a></td> 
                           <td style="text-transform: uppercase; font-size: 15px; vertical-align: middle;"><a href="javascript:void(0);" onclick="confirmDelete(<?= $client['PRC_IDPROSPEC_CLIENTES']; ?>)"><span class="label label-danger"><i class="fa fa-trash"></i></span></a></td>         
                       </tr>
