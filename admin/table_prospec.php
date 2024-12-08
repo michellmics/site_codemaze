fdae3640b266f5d23900a670c79c2d445ef503db
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 18:03:23 +0000

    deploy

--- admin/table_prospec.php
+++ admin/table_prospec.php
@@ -80,6 +80,12 @@ else
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="https://www.codemaze.com.br/site/logo_small_site.png">
+    <meta name="apple-mobile-web-app-title" content="Codemaze">
+
         <!-- ######################################################## --> 
     <!-- SWEETALERT 2 --> 
     <!-- SweetAlert2 CSS -->
