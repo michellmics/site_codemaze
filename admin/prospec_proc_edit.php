d7ea37282616efeed2f4d64fce41ec006f6170c7
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-08 22:08:44 +0000

    deploy

---
+++ admin/prospec_proc_edit.php
@@ -0,0 +1,69 @@
+<?php
+include_once 'objetos.php'; // Carrega a classe de conexão e objetos
+
+session_start(); // Inicia a sessão para armazenar dados do usuário
+
+
+class registerProspec extends SITE_ADMIN
+{
+    public function updateProspec($nome,
+    $endereco, 
+    $maps, 
+    $contato, 
+    $telefone, 
+    $email,
+    $data,
+    $status,
+    $obs)
+    {
+        try {
+            // Cria conexão com o banco de dados
+            if (!$this->pdo) {
+                $this->conexao();
+            }
+
+            $result = $this->updateProspecInfo($nome,
+            $endereco, 
+            $maps, 
+            $contato, 
+            $telefone, 
+            $email,
+            $data,
+            $status,
+            $obs);
+
+            echo "Cliente atualizado com sucesso.";
+                                      
+        } catch (PDOException $e) {  
+            echo "ERRO: Não foi possível atualizar o Cliente.";
+        } 
+    }
+}
+
+// Processa a requisição POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    
+    $nome = $_POST['nome']; 
+    $endereco = $_POST['endereco']; 
+    $maps = $_POST['maps']; 
+    $contato = $_POST['contato']; 
+    $telefone = $_POST['telefone']; 
+    $email = $_POST['email']; 
+    $data = $_POST['data']; 
+    $status = $_POST['status']; 
+    $obs = $_POST['obs'];
+    $registerProspec = new registerProspec();
+    
+    $result = $registerProspec->updateProspec($nome,
+        $endereco, 
+        $maps, 
+        $contato, 
+        $telefone, 
+        $email,
+        $data,
+        $status,
+        $obs
+    );
+}
+?>
