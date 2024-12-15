44c87c2db31d45c4aceb2e8cd5f58606d3cc95bf
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-15 22:11:39 +0000

    deploy

--- whois/index.php
+++ whois/index.php
@@ -53,11 +53,11 @@
     $responseStatus = $matches[1]; // O número capturado estará no índice 1
     if($responseStatus == "2")
     {
-      echo "O domínio $fqdn está indisponível para registro.";
+      echo "O domínio <b>$fqdn<b> está <b>indisponível<b> para registro.";
     }
     else
       {
-        echo "O domínio $fqdn está disponível para registro.";
+        echo "O domínio <b>$fqdn</b> está <b>disponível<b> para registro.";
       }
 
   } else {
