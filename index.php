3fc61f7a1fbf82cba75f62d8ba87ebcc640b3db3
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-15 21:52:15 +0000

    deploy

--- index.php
+++ index.php
@@ -219,7 +219,7 @@
         <input id="domainInput" type="text" placeholder="Digite o domínio..." 
             style="width: 300px; padding: 10px; font-size: 18px; border: none; border-radius: 5px; margin-right: 10px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);">
         <button id="checkButton" style="padding: 12px 25px; font-size: 18px; color: white; background-color: rgb(248, 80, 2); border: none; border-radius: 5px; cursor: pointer; transition: all 0.3s ease;">
-            Verificar Disponibilidade
+            Verificar
         </button>
     </div>
 </div>
