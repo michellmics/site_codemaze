d49fc99323b0f90336d2da5e001248afa73655fb
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-15 21:25:46 +0000

    deploy

--- index.php
+++ index.php
@@ -212,7 +212,7 @@
     <div style="max-width: 1200px; margin: 0 auto; text-align: center; font-family: Arial, sans-serif;">
         <input type="text" placeholder="Digite o domínio..." 
             style="width: 300px; padding: 10px; font-size: 18px; border: none; border-radius: 5px; margin-right: 10px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);">
-        <span style="color: white; font-size: 20px; font-weight: bold; margin-right: 10px;">.com.br</span>
+        <span style="color: white; font-size: 20px; margin-right: 10px;">.com.br</span>
         <button style="padding: 12px 25px; font-size: 18px; color: white; background-color: rgb(248, 80, 2); border: none; border-radius: 5px; cursor: pointer; transition: all 0.3s ease;">
             Verificar Disponibilidade
         </button>
