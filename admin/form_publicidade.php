d9e3ecddaa432e598f44b13a1ab3b23decd3d2fa
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2024-12-31 00:10:01 +0000

    deploy

--- admin/form_publicidade.php
+++ admin/form_publicidade.php
@@ -47,7 +47,7 @@
     <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
 
         <!-- jQuery 2.1.3 -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.8/dist/inputmask.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
