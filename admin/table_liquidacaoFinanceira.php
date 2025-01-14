66db068f612e8009ead66448abdbccf13d7d02a5
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-14 22:30:44 +0000

    deploy

--- admin/table_liquidacaoFinanceira.php
+++ admin/table_liquidacaoFinanceira.php
@@ -3,6 +3,14 @@
   include 'modal.php';
   
   session_start(); 
+  if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > SESSION_TIMEOUT)) {
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+$_SESSION['last_activity'] = time();
+
   define('SESSION_TIMEOUT', 1800); // 30 minutos
 
   if (!isset($_SESSION['user_id'])) 
