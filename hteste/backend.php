e705b44e01d0d300757e5806cd5d16b2674f296a
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-01-25 10:48:35 +0000

    deploy

---
+++ hteste/backend.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+API do cliente disponível em:
+https://clientapi.mrrodz.com/
+
+Utilizar as credenciais cadastradas no banco do cliente
+senha 123456
+enviar o clientId 1 no header
+
+Documentação do Swagger:
+https://clientapi.mrrodz.com/api-docs
+*/
+
+
+?>
