cf40e916e0eed4afd3147bcd1dd822f13b1e203d
Michell Duarte <127140593+michellmics@users.noreply.github.com>
2025-02-04 12:41:05 +0000

    deploy

--- index.php
+++ index.php
@@ -850,7 +850,7 @@ Com nossa expertise, ajudamos sua empresa a integrar diferentes fontes de dados 
     <!-- ALL JS FILES -->
     <script src="js/all.js"></script>
     <!-- ALL PLUGINS -->
-    <script src="js/custom.js"></script>
+    <script src="js/custom.js?v=<?php echo time(); ?>"></script>
     <script src="js/portfolio.js"></script>
     <script src="js/hoverdir.js"></script>    
 
